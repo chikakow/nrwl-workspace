6d494b1c0944430bb62f064609d0254ffc2faff1
Chikako Wakishima <chikakow@gmail.com>
2020-02-26 09:45:20 -0500

    nrwl upto tutorial 3

---
+++ apps/todos/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+interface Todo {
+  title: string;
+}
+
+@Component({
+  selector: 'nrwl-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`
+    });
+  }
+}
